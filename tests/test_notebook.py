188710b4b09370b705660b773b0d370fa87f84b9
Tom Bland <23723407+tsmbland@users.noreply.github.com>
2023-10-11 17:49:38 +0100

    added notebook test

---
+++ tests/test_notebook.py
@@ -0,0 +1,19 @@
+import pytest
+import os
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+
+class TestDeCompletion:
+    """
+    Testing that the notebook runs to completion
+
+    """
+
+    def test_1(self):
+        f = os.path.dirname(os.path.realpath(__file__)) + "/../scripts/Tutorial.ipynb"
+        run_path = os.path.dirname(f)
+        with open(f) as file:
+            nb = nbformat.read(file, as_version=4)
+            ep = ExecutePreprocessor(kernel_name="python3")
+            out = ep.preprocess(nb, {"metadata": {"path": run_path}})
