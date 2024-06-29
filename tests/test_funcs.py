464e20c4b4cc88b1b56a812d5f3068300779bc88
Tom Bland <23723407+tsmbland@users.noreply.github.com>
2024-06-29 17:44:32 +0100

    Replace cv2 with skimage

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -13,8 +13,8 @@ def test_make_mask():
         os.path.dirname(os.path.abspath(__file__)) + "/../scripts/nwg338_ROI_manual.txt"
     )
     result = make_mask(img.shape, roi)
-    assert np.sum(~np.isnan(result)) == 17268
-    assert np.sum(~np.isnan(result[300, :])) == 179
+    assert np.sum(~np.isnan(result)) == 17033
+    assert np.sum(~np.isnan(result[300, :])) == 178
 
 
 def test_polycrop():
@@ -26,5 +26,6 @@ def test_polycrop():
         os.path.dirname(os.path.abspath(__file__)) + "/../scripts/nwg338_ROI_manual.txt"
     )
     result = polycrop(img, roi, 0)
-    assert np.count_nonzero(result) == 17268
-    assert np.count_nonzero(result[300, :]) == 179
+    assert int(result.sum()) == 105625425
+    assert np.count_nonzero(result) == 17033
+    assert np.count_nonzero(result[300, :]) == 178
